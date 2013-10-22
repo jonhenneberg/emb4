aad3445ca76e803d00d952a3cbf42a0483f20703
Jon Henneberg <jon@klan-nii.dk>
2013-10-22 20:39:30 +0200

    Update gpio-test.c

--- gpio-test/gpio-test.c
+++ gpio-test/gpio-test.c
@@ -7,49 +7,62 @@
 #include <linux/device.h>
 #include <linux/cdev.h>
 #include <asm/uaccess.h>
+#include <linux/interrupt.h>
 #include <linux/gpio.h>
  
-static dev_t first; // Global variable for the first device number 
-static struct cdev c_dev; // Global variable for the character device structure
-static struct class *cl; // Global variable for the device class
+#define DRV_NAME           "gpio-test"
 
-char c;
+static dev_t first; 		// Global variable for the first device number 
+static struct cdev c_dev; 	// Global variable for the character device structure
+static struct class *cl; 	// Global variable for the device class
 
-static int my_open(struct inode *i, struct file *f)
-{
-  printk(KERN_INFO "Driver: open()\n");
+
+struct gpio_irq_test {
+   u16 irq;
+   u16 irq_pin, gpio_pin, led_pin;
+};
+
+static struct gpio_irq_test test_data;
+
+
+static int my_open(struct inode *i, struct file *f) {
+  printk(KERN_INFO DRV_NAME " : Driver: open()\n");
   return 0;
 }
-static int my_close(struct inode *i, struct file *f)
-{
-  printk(KERN_INFO "Driver: close()\n");
+
+static int my_close(struct inode *i, struct file *f) {
+  printk(KERN_INFO DRV_NAME " : Driver: close()\n");
   return 0;
 }
  
 static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off) {
 	char c[2];
-	printk(KERN_INFO "Driver: read()\n");
-	c[0] = gpio_get_value(46) + 0x30;
-	c[1] = '\n';
+	if (*off == 0) {
+		printk(KERN_INFO DRV_NAME " : Driver: read()\n");
+		c[0] = gpio_get_value(test_data.irq_pin) + 0x30;
+		c[1] = '\n';
 
-	if (copy_to_user(buf, &c, 2))
-		return -EFAULT;
-	else
-	   return 2;
-
+		if (copy_to_user(buf, &c, 2)) {
+			return -EFAULT;
+		} else	{
+			(*off)++;
+			return 2;
+		}
+	} else {
+		return 0;
+	}
 }
-static ssize_t my_write(struct file *f, const char __user *buf, size_t len, loff_t *off)
-{
+static ssize_t my_write(struct file *f, const char __user *buf, size_t len, loff_t *off) {
 	char c;
-	printk(KERN_INFO "Driver: write()\n");	
+	printk(KERN_INFO DRV_NAME " : Driver: write()\n");	
 	if (copy_from_user(&c, buf, 1))	return -EFAULT;
 
 	if (buf[0] == '1') {
-		gpio_set_value(38, 1);   
-		gpio_set_value(54, 1);  
+		gpio_set_value(test_data.gpio_pin, 1);
+		gpio_set_value(test_data.led_pin, 1); 
 	} else {
-		gpio_set_value(38, 0);
-		gpio_set_value(54, 0);  
+		gpio_set_value(test_data.gpio_pin, 0);
+		gpio_set_value(test_data.led_pin, 1); 
 	}
 
 	return len;
@@ -66,92 +79,73 @@ static struct file_operations pugs_fops =
  
 static int __init ofcd_init(void) /* Constructor */
 {
-  printk(KERN_INFO "Namaskar: ofcd registered\n");
-  if (alloc_chrdev_region(&first, 0, 1, "JON") < 0)
-  {
-    return -1;
-  }
-    if ((cl = class_create(THIS_MODULE, "chardrv")) == NULL)
-  {
-    unregister_chrdev_region(first, 1);
-    return -1;
-  }
-    if (device_create(cl, NULL, first, NULL, "gpiotest") == NULL)
-  {
-    class_destroy(cl);
-    unregister_chrdev_region(first, 1);
-    return -1;
-  }
-    cdev_init(&c_dev, &pugs_fops);
-    if (cdev_add(&c_dev, first, 1) == -1)
-  {
-    device_destroy(cl, first);
-    class_destroy(cl);
-    unregister_chrdev_region(first, 1);
-    return -1;
-  }
-//	if (gpio_is_valid(38)) {
-//		printk(KERN_INFO "GPIO: invalid GPIO 38\n");
-//		return -1;
-//	}
-//	if (gpio_is_valid(54)) {
-//		printk(KERN_INFO "GPIO: invalid GPIO 54\n");
-//		return -1;
-//	}
-//	if (gpio_is_valid(46)) {
-//		printk(KERN_INFO "GPIO: invalid GPIO 46\n");
-//		return -1;
-//	}
+	int err;
+	printk(KERN_INFO DRV_NAME " : Registered\n");
 
-	if (gpio_request(38, "led out")) {
-		printk(KERN_INFO "GPIO: unable to register GPIO38\n");
-		device_destroy(cl, first);
-		class_destroy(cl);
-		unregister_chrdev_region(first, 1);
-		return -1;
-	}
-	if(gpio_request(54, "led out_usr")) {
-		device_destroy(cl, first);
-		class_destroy(cl);
-		unregister_chrdev_region(first, 1);
-		gpio_free(38);
-		printk(KERN_INFO "GPIO: unable to register GPIO54\n");
-		return -1;
-	}
-	if (gpio_request(46, "btn in")) {
-		device_destroy(cl, first);
-		class_destroy(cl);
-		unregister_chrdev_region(first, 1);
-		gpio_free(38);
-		gpio_free(46);
-		printk(KERN_INFO "GPIO: unable to register GPIO46\n");
-		return -1;
+	test_data.irq_pin = 46;
+	test_data.gpio_pin = 38;
+	test_data.led_pin = 54;
+
+	if (alloc_chrdev_region(&first, 0, 1, "JON") < 0) { goto err_return;	}
+	if ((cl = class_create(THIS_MODULE, "chardrv")) == NULL) { goto err_unregister_chrdev_return; }
+	if (device_create(cl, NULL, first, NULL, "gpiotest") == NULL) { goto err_class_destroy_return; }
+	cdev_init(&c_dev, &pugs_fops);
+	if (cdev_add(&c_dev, first, 1) == -1) { goto err_device_destroy_return; }
+
+	/* Confiure GPIO Pins*/
+	if (gpio_request(test_data.gpio_pin, "out pin")) {
+		printk(KERN_ALERT DRV_NAME " : Unable to register output pin %d\n", test_data.gpio_pin);
+		goto err_device_destroy_return;
 	}
 
-		gpio_direction_output(38, 0);
-		gpio_direction_output(54, 0);
-		gpio_direction_input(46);
+	if(gpio_request(test_data.led_pin, "led pin")) {
+		printk(KERN_ALERT DRV_NAME " : Unable to register led pin %d\n", test_data.led_pin);
+		goto err_free_outpin_return;
+	}
 
+	if (gpio_request(test_data.irq_pin, "irq pin")) {
+		printk(KERN_ALERT DRV_NAME " : Unable to register irq pin %d\n", test_data.led_pin);
+		goto err_free_ledpin_return;
+	}
 
-  return 0;
+	gpio_direction_output(test_data.led_pin, 0);
+	gpio_direction_output(test_data.gpio_pin, 0);
+	gpio_direction_input(test_data.irq_pin);
+
+	return 0;
+
+/* Cleanup of registers */	
+err_free_ledpin_return:
+	gpio_free(test_data.led_pin);
+err_free_outpin_return:
+	gpio_free(test_data.gpio_pin);
+err_device_destroy_return:	
+	device_destroy(cl, first);
+err_class_destroy_return:
+	class_destroy(cl);
+err_unregister_chrdev_return:
+	unregister_chrdev_region(first, 1);
+err_return:
+	return -1;
 }
  
 static void __exit ofcd_exit(void) /* Destructor */
 {
+	/* Cleanup GPIO pins*/
+	gpio_free(test_data.led_pin);
+	gpio_free(test_data.gpio_pin);
+	gpio_free(test_data.irq_pin);
 
-	gpio_free(38);
-	gpio_free(54);
-	gpio_free(46);
-
-  cdev_del(&c_dev);
-  device_destroy(cl, first);
-  class_destroy(cl);
-  unregister_chrdev_region(first, 1);
-  printk(KERN_INFO "Alvida: ofcd unregistered\n");
+	/* Cleanup charecter devices*/
+	cdev_del(&c_dev);
+	device_destroy(cl, first);
+	class_destroy(cl);
+	unregister_chrdev_region(first, 1);
+	printk(KERN_INFO DRV_NAME " : Unregistered\n");
 }
  
 module_init(ofcd_init);
 module_exit(ofcd_exit);
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Anil Kumar Pugalia <email_at_sarika-pugs_dot_com>");
-MODULE_DESCRIPTION("Our First Character Driver");
+MODULE_AUTHOR("Jon Henneberg");
+MODULE_DESCRIPTION("GPIO Test");
