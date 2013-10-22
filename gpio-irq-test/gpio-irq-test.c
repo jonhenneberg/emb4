15d886a10be0228aa936f1604569ebb36946028c
Jon Henneberg <jon@klan-nii.dk>
2013-10-22 20:39:51 +0200

    Update gpio-irq-test.c

--- gpio-irq-test/gpio-irq-test.c
+++ gpio-irq-test/gpio-irq-test.c
@@ -95,35 +95,37 @@ static int __init ofcd_init(void) /* Constructor */
 	test_data.led_pin = 54;
 
 	if (alloc_chrdev_region(&first, 0, 1, "JON") < 0) { goto err_return;	}
-	if ((cl = class_create(THIS_MODULE, "chardrv")) == NULL) { goto unregister_chrdev_return; }
-	if (device_create(cl, NULL, first, NULL, "gpiotest") == NULL) { goto class_destroy_return; }
+	if ((cl = class_create(THIS_MODULE, "chardrv")) == NULL) { goto err_unregister_chrdev_return; }
+	if (device_create(cl, NULL, first, NULL, "gpiotest") == NULL) { goto err_class_destroy_return; }
 	cdev_init(&c_dev, &pugs_fops);
-	if (cdev_add(&c_dev, first, 1) == -1) { goto device_destroy_return; }
+	if (cdev_add(&c_dev, first, 1) == -1) { goto err_device_destroy_return; }
 
+	/* Confiure GPIO Pins*/
 	if (gpio_request(test_data.gpio_pin, "out pin")) {
 		printk(KERN_ALERT DRV_NAME " : Unable to register output pin %d\n", test_data.gpio_pin);
-		goto device_destroy_return;
+		goto err_device_destroy_return;
 	}
 
 	if(gpio_request(test_data.led_pin, "led pin")) {
 		printk(KERN_ALERT DRV_NAME " : Unable to register led pin %d\n", test_data.led_pin);
-		goto free_outpin_return;
+		goto err_free_outpin_return;
 	}
 
 	if (gpio_request(test_data.irq_pin, "irq pin")) {
 		printk(KERN_ALERT DRV_NAME " : Unable to register irq pin %d\n", test_data.led_pin);
-		goto free_ledpin_return;
+		goto err_free_ledpin_return;
 	}
 
 	gpio_direction_output(test_data.led_pin, 0);
 	gpio_direction_output(test_data.gpio_pin, 0);
 	gpio_direction_input(test_data.irq_pin);
 	
+	/* Confiure IRQ*/
 
    err = gpio_to_irq(test_data.irq_pin);
    if (err < 0) {
       printk(KERN_ALERT DRV_NAME " : failed to get IRQ for pin %d.\n", test_data.irq_pin);
-      goto free_irqpin_return;
+      goto err_free_irqpin_return;
    } else {
       test_data.irq = (u16)err;
       err = 0;
@@ -132,38 +134,41 @@ static int __init ofcd_init(void) /* Constructor */
 	err = request_any_context_irq(test_data.irq, gpio_test_irq_interrupt_handler, IRQF_TRIGGER_RISING | IRQF_DISABLED, DRV_NAME, (void*)&test_data);
    if (err < 0) {
       printk(KERN_ALERT DRV_NAME " : failed to enable IRQ %d for pin %d.\n", test_data.irq, test_data.irq_pin);
-      goto free_irqpin_return;
+      goto err_free_irqpin_return;
    } else
       test_data.irq_enabled = 1;
 
 
 	return 0;
 
-	/* Cleanup of registers */	
-	free_irqpin_return:
-		gpio_free(test_data.irq_pin);
-	free_ledpin_return:
-		gpio_free(test_data.led_pin);
-	free_outpin_return:
-		gpio_free(test_data.gpio_pin);
-	device_destroy_return:	
-   	device_destroy(cl, first);
-	class_destroy_return:
-   	class_destroy(cl);
-	unregister_chrdev_return:
-		unregister_chrdev_region(first, 1);
-	err_return:
-		return -1;
+/* Cleanup of registers */	
+err_free_irqpin_return:
+	gpio_free(test_data.irq_pin);
+err_free_ledpin_return:
+	gpio_free(test_data.led_pin);
+err_free_outpin_return:
+	gpio_free(test_data.gpio_pin);
+err_device_destroy_return:	
+	device_destroy(cl, first);
+err_class_destroy_return:
+	class_destroy(cl);
+err_unregister_chrdev_return:
+	unregister_chrdev_region(first, 1);
+err_return:
+	return -1;
 }
  
 static void __exit ofcd_exit(void) /* Destructor */
 {
+	/* Cleanup IRQ*/
 	free_irq(test_data.irq, (void*)&test_data);
 
+	/* Cleanup GPIO pins*/
 	gpio_free(test_data.led_pin);
 	gpio_free(test_data.gpio_pin);
 	gpio_free(test_data.irq_pin);
 
+	/* Cleanup charecter devices*/
 	cdev_del(&c_dev);
 	device_destroy(cl, first);
 	class_destroy(cl);
